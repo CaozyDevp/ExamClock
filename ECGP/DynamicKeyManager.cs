3ce53ef9f107ebb0c927981cf02ba101fbcec907
Zachary Cao <caozydev@qq.com>
2026-03-07 22:58:13 +0800

    feat: add ClientResponder class

---
+++ ECGP/DynamicKeyManager.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Copyright(C) 2025 Zachary Cao (CaozyDevp)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace ECGP
+{
+    /// <summary>
+    /// 动态密钥管理类
+    /// </summary>
+    public class DynamicKeyManager : IDisposable
+    {
+        /// <summary>
+        /// 保存5个可用密钥，每个密钥的失效时间间隔1s
+        /// </summary>
+        private byte[][] _keys = new byte[5][];
+
+        /// <summary>
+        /// 指向最新的密钥
+        /// </summary>
+        private int _pointer = 0;
+
+        private readonly Timer _timer;
+
+        /// <summary>
+        /// 最新的密钥
+        /// </summary>
+        public byte[] LatestKey => _keys[_pointer];
+
+        private object _updateLock = new object();
+
+        /// <summary>
+        /// 更新密钥
+        /// </summary>
+        private void Update()
+        {
+            lock (_updateLock)
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 128;
+                    aes.GenerateKey();
+                    int next = (_pointer + 1) % _keys.Length;
+                    _keys[next] = aes.Key;
+                    _pointer = next;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 检查密钥是否有效
+        /// </summary>
+        /// <param name="key">需要检查的密钥</param>
+        /// <returns>是否有效</returns>
+        public bool CheckIfValid(byte[] key)
+        {
+            foreach (var k in _keys)
+            {
+                if (k != null && key != null && k.SequenceEqual(key)) return true;
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        public DynamicKeyManager()
+        {
+            // 装填初始密钥
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                Update();
+            }
+
+            //初始化每秒更新机制
+            _timer = new Timer((e) =>
+            {
+                Update();
+            }, null, 1000, 1000);
+        }
+    }
+}
---
+++ ECGP/Responders/ClientResponder.cs
@@ -0,0 +1,208 @@
+﻿/*
+ * Copyright(C) 2025 Zachary Cao (CaozyDevp)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ECGP.Messages;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ECGP.Responders
+{
+    /// <summary>
+    /// 客户端的主响应器，用于回复动态密钥、控制指令确认报文等信息
+    /// </summary>
+    public class ClientResponder : IDisposable
+    {
+        private UdpClient _udpClient;
+
+        /// <summary>
+        /// 接收请求的端口号
+        /// </summary>
+        public int Port
+        {
+            get => _port;
+            set
+            {
+                if (value >= 1 && value <= 65535)
+                {
+                    _port = value;
+                    return;
+                }
+                throw new ArgumentException("Invalid port number!", nameof(value));
+            }
+        }
+        private int _port = 25584;
+
+        /// <summary>
+        /// RSA公钥xml
+        /// </summary>
+        public string RsaPublicKeyXml
+        {
+            get => _rsaPublicKeyXml;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                _rsaPublicKeyXml = value;
+            }
+        }
+        private string _rsaPublicKeyXml;
+
+        private DynamicKeyManager _keyManager = new DynamicKeyManager();
+
+        /// <summary>
+        /// 执行指定命令的委托
+        /// </summary>
+        public Func<InstructionType, byte[], ReturnCode> ExecuteInstruction
+        {
+            get => _executeInstruction;
+            set
+            {
+                _executeInstruction = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
+        private Func<InstructionType, byte[], ReturnCode> _executeInstruction;
+
+        /// <summary>
+        /// 显示执行结果的委托
+        /// </summary>
+        public Action<ReturnCode> ShowExecutionResult
+        {
+            get => _showExecutionResult;
+            set
+            {
+                _showExecutionResult = value;
+            }
+        }
+        private Action<ReturnCode> _showExecutionResult;
+
+        /// <summary>
+        /// 是否已经启动
+        /// </summary>
+        public bool IsRunning { get; private set; }
+
+        public ClientResponder(string rsaPublicKeyXml)
+        {
+            RsaPublicKeyXml = rsaPublicKeyXml;
+        }
+
+        public ClientResponder(string rsaPublicKeyXml, int port)
+        {
+            RsaPublicKeyXml = rsaPublicKeyXml;
+            Port = port;
+        }
+
+        public async Task StartAsync()
+        {
+            if (IsRunning) return;
+            IsRunning = true;
+
+            _udpClient?.Close();
+            _udpClient = new UdpClient(Port);
+
+            while (IsRunning)
+            {
+                try
+                {
+                    var result = await _udpClient.ReceiveAsync();
+                    var data = result.Buffer;
+                    var endPoint = result.RemoteEndPoint;
+
+                    // 如果是本机发来的请求，则忽略
+                    if (IsLocalAddress(endPoint.Address)) continue;
+
+                    var message = ECGPMessage.Parse(data);
+                    if (message.Type == 0x03)       // 动态密钥请求报文
+                    {
+                        if (message is DynamicKeyRequestMessage)
+                        {
+                            uint number = message.Number;
+                            ReplyDynamicKey(endPoint, number);
+                        }
+                    }
+                    else if (message.Type == 0x05)  // 控制指令报文
+                    {
+                        if (message is InstructionMessage msg)
+                        {
+                            var ret = ExecuteInstruction(msg.CommandCode, msg.Parameters);
+                            ShowExecutionResult?.Invoke(ret);
+                            ReplyConfirmation(endPoint, ret, msg.Number);
+                        }
+                    }
+                }
+                catch
+                {
+                    // [TODO] 这里可以log一下，暂且忽略
+                }
+            }
+        }
+
+        /// <summary>
+        /// 回复动态密钥响应报文
+        /// </summary>
+        /// <param name="target">目标主机</param>
+        /// <param name="numReceived">接收到的请求报文的特征码</param>
+        private void ReplyDynamicKey(IPEndPoint target, uint numReceived)
+        {
+            var dynamicKey = _keyManager.LatestKey;
+            var message = new DynamicKeyResponseMessage(dynamicKey, numReceived, RsaPublicKeyXml);
+            var bytes = message.ToBytes();
+
+            using (UdpClient sender = new UdpClient())
+            {
+                sender.Send(bytes, bytes.Length, target);
+            }
+        }
+
+        /// <summary>
+        /// 回复控制指令确认报文
+        /// </summary>
+        /// <param name="target">目标主机</param>
+        /// <param name="returnCode">返回值</param>
+        private void ReplyConfirmation(IPEndPoint target, ReturnCode returnCode, uint numReceived)
+        {
+            var message = new ConfirmationMessage(numReceived, returnCode, RsaPublicKeyXml);
+            var bytes = message.ToBytes();
+
+            using (UdpClient sender = new UdpClient())
+            {
+                sender.Send(bytes, bytes.Length, target);
+            }
+        }
+
+        private bool IsLocalAddress(IPAddress address)
+        {
+            var hostAddresses = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (var addr in hostAddresses)
+            {
+                if (addr.Equals(address))
+                    return true;
+            }
+            return IPAddress.IsLoopback(address);
+        }
+
+        public void Dispose()
+        {
+            IsRunning = false;
+            _udpClient?.Close();
+            _udpClient = null;
+        }
+    }
+}
