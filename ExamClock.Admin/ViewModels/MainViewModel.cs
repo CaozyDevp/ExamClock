0ee74605c545f288d51a9499ad2d553f3d1943a4
Zachary Cao <caozydev@qq.com>
2026-03-20 21:41:01 +0800

    feat: improve password input handling in MainViewModel

--- ExamClock.Admin/ViewModels/MainViewModel.cs
+++ ExamClock.Admin/ViewModels/MainViewModel.cs
@@ -301,7 +301,7 @@ namespace ExamClock.Admin.ViewModels
         /// <returns>用户输入的密码字符串，可能为null</returns>
         private string GetPasswordInput()
         {
-            var dialog = new PasswordInputWindow(Username);
+            var dialog = new PasswordInputWindow(string.IsNullOrEmpty(Username) ? "未设置" : Username);
             try
             {
                 if (dialog.ShowDialog() == true)
